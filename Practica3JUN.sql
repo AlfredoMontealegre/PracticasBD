cb02001beafc73cf1b44b90f26f48b763d53b1bf
AlfredoMontealegre <montealfredo24@gmail.com>
2026-06-03 14:03:25 -0600

    ModuloDos

--- Practica3JUN.sql
+++ Practica3JUN.sql
@@ -1,48 +1,76 @@
-use master 
+use master
 go
 
--- Modulo 1, empezamos con la creacion de la base de datos.
-
-create database HospitalDB
+-- Eliminacion de Base de datos por si ya existe
+if exists (select * from sys.databases where name = 'hospital_db')
+	drop database hospital_db
 go
 
-use HospitalDB
+create database hospital_db
 go
 
--- Creacion de las tablas (como en el modulo 1).
+use hospital_db
+go
+
+-- Modulo 1, creacion de la base de datos.
 
 -- Tabla pacientes
-create table Pacientes(
+create table pacientes(
+	id_paciente int identity(1,1) primary key,
+	nombre_paciente nvarchar(60) not null,
+	genero bit not null,
+	correo nvarchar(50) not null
+);
+go
+
+-- Tabla especialidades 
+create table especialidades(
+	id_especialidad int identity(1,1) primary key,
+	nombre nvarchar(50) not null
 );
 go
 
 -- Tabla medicos
-create table Medicos(
-);
-go
-
--- Tabla especialidades
-create table Especialidades(
+create table medicos(
+	id_medico int identity(1,1) primary key,
+	nombre_medico nvarchar(60) not null,
+	correo nvarchar(50) not null,
+	salario decimal(10,2) not null,
+	id_especialidad int
 );
 go
 
 -- Tabla citas
-create table Citas(
+create table citas(
+	id_cita int identity(1,1) primary key,
+	fecha datetime not null,
+	id_paciente int,
+	id_medico int
 );
 go
 
 -- Tabla habitaciones
-create table Habitaciones(
+create table habitaciones(
+	id_habitacion int identity(1,1) primary key,
+	numero int not null,
+	id_paciente int
 );
 go
 
 -- Tabla tratamientos
-create table Tratamientos(
+create table tratamientos(
+	id_tratamiento int identity(1,1) primary key,
+	descripcion nvarchar(100) not null,
+	id_paciente int
 );
 go
 
--- Tabla Medicamentos
-create table Medicamentos(
+-- Tabla medicamentos
+create table medicamentos(
+	id_medicamento int identity(1,1) primary key,
+	nombre nvarchar(50) not null,
+	id_tratamiento int
 );
 go
+
 
