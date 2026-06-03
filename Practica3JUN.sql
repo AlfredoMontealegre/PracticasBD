1713f9decd190272178af4a329720ffa06f7a4a0
AlfredoMontealegre <montealfredo24@gmail.com>
2026-06-03 13:59:30 -0600

    PrimerModulo

---
+++ Practica3JUN.sql
@@ -0,0 +1,48 @@
+use master 
+go
+
+-- Modulo 1, empezamos con la creacion de la base de datos.
+
+create database HospitalDB
+go
+
+use HospitalDB
+go
+
+-- Creacion de las tablas (como en el modulo 1).
+
+-- Tabla pacientes
+create table Pacientes(
+);
+go
+
+-- Tabla medicos
+create table Medicos(
+);
+go
+
+-- Tabla especialidades
+create table Especialidades(
+);
+go
+
+-- Tabla citas
+create table Citas(
+);
+go
+
+-- Tabla habitaciones
+create table Habitaciones(
+);
+go
+
+-- Tabla tratamientos
+create table Tratamientos(
+);
+go
+
+-- Tabla Medicamentos
+create table Medicamentos(
+);
+go
+
